779b0dc3f068ad5e39b3894aa25ac6ab35903528
berkayoztunc <mailberkayoztunc@gmail.com>
2023-03-17 12:47:17 +0300

    Update boilerplate

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { RouterLink, RouterView } from 'vue-router';
 import SideMenu from '@/components/SideMenu.vue';
 import NavMenu from '@/components/NavMenu.vue';
 import Footer from '@/components/Footer.vue';
+import { useSideMenuMy } from './stores/counter';
+
+const side = useSideMenuMy();
 </script>
 
 <template>
@@ -10,23 +13,9 @@ import Footer from '@/components/Footer.vue';
   <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
     <SideMenu />
 
-    <div
-      class="fixed inset-0 z-10 hidden bg-gray-900/50 dark:bg-gray-900/90"
-      id="sidebarBackdrop"
-    ></div>
+    <div class="fixed inset-0 z-10 hidden bg-gray-900/50 dark:bg-gray-900/90" id="sidebarBackdrop"></div>
 
-    <div
-      id="main-content"
-      class="
-        relative
-        w-full
-        h-full
-        overflow-y-auto
-        bg-gray-50
-        lg:ml-64
-        dark:bg-gray-900
-      "
-    >
+    <div id="main-content" :class="side.side ? '' : 'lg:ml-64'" class="relative w-full h-full overflow-y-auto bg-gray-50 dark:bg-gray-900">
       <main>
         <div class="px-4 pt-6">
           <div class="grid gap-4 xl:grid-cols-2 2xl:grid-cols-3">
